01bb33bee01f611ef9617ba09f08b197964fd4d8
Ayush Singh <singhayush.2711@gmail.com>
2022-03-17 13:22:02 +0530

    Update trunc.go

--- Getting Started with Go/trunc.go
+++ Getting Started with Go/trunc.go
@@ -1,1 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num float64
+	var result int64
+	fmt.Println("Enter a floating piont number")
+	fmt.Scan(&num)
+	result = int64(num)
+	fmt.Println("Entered number is ", num)
+	fmt.Println("Truncated number is ", result)
+}
 
